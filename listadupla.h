aa0ffdbd8312f8805bc4d81fc390bf6c97a88d84
Sabrina Poderis <sabrinapoderis@gmail.com>
2020-01-13 18:26:53 -0300

    Adicionadas novas funções

--- listadupla.h
+++ listadupla.h
@@ -4,17 +4,17 @@ typedef struct lista2{
 	struct lista2 *prox;
 }Lista2;
 
-/*inicializa uma lista dupla*/
+/* inicializa uma lista dupla */
 Lista2 * lista_cria(){
 	return NULL;
 }
 
-/* retorna 1 se vazia ou zero se falso*/
+/* retorna 1 se vazia ou zero se falso */
 int lista_vazia(Lista2* li){
 	return(li==NULL);
 }
 
-/* insere o novo dado no início da lista  e retorna a lista atualizada*/
+/* insere o novo dado no início da lista  e retorna a lista atualizada */
 Lista2* lista_insere( Lista2* li, int dado){
 	Lista2 *novo = (Lista2*) malloc (sizeof(Lista2));
 	novo->info=dado;
@@ -25,20 +25,35 @@ Lista2* lista_insere( Lista2* li, int dado){
 	return novo;
 }
 
-/* percorre a lista do inicio até o fim mostrando os elementos*/
+/* percorre a lista do inicio até o fim mostrando os elementos */
 void lista_exibe(Lista2* li) {
 	Lista2 *aux;
-	printf("\n\nLista\n\n");
+	printf("\n\n-------- Lista --------\n\n");
 	if(lista_vazia(li)){
-		printf("Sem elementos no momento");
+		printf("(Sem elementos no momento)");
 	}else{
 		for(aux=li;aux!=NULL;aux=aux->prox)
     	printf("%d\t", aux->info);
 	}
-	printf("\n\n");
-}       
+	printf("\n\n-----------------------\n\n");
+}   
 
-/* retorna o elemento ou NULL se não achou*/
+/* exibe lista reversa */
+void lista_exibe_reverso(Lista2 *li) {
+	Lista2 *aux, *aux1;
+	printf("\n\n --------Lista Reversa --------\n\n");
+	if(lista_vazia(li)){
+		printf("(Sem elementos no momento)");
+	}else{
+		for(aux=li; aux->prox!=NULL; aux=aux->prox);		
+		for(aux1=aux; aux1!=NULL; aux1=aux1->ant){
+			printf("%d\t", aux1->info);
+		}	
+	}
+	printf("\n\n-----------------------\n\n");
+}    
+
+/* retorna o elemento ou NULL se não achou */
 Lista2* lista_busca(Lista2* li, int dado) {
 	Lista2 *a; 
 	for(a=li;a!=NULL;a=a->prox)
@@ -46,7 +61,7 @@ Lista2* lista_busca(Lista2* li, int dado) {
 	return NULL;
 }
   
-/* retorna o novo inicio ou a lista original*/
+/* retorna o novo inicio ou a lista original */
 Lista2* lista_retira(Lista2* li, int dado){
 	//procura o elemento na lista usando a função busca
 	Lista2* aux=lista_busca(li, dado);
@@ -64,13 +79,91 @@ Lista2* lista_retira(Lista2* li, int dado){
 	return li;
 }
 
-/* libera o espaço ocupado pela lista*/
-void lista_libera(Lista2 *li){
+/* libera o espaço ocupado pela lista */
+void lista_esvazia(Lista2 *li){
     Lista2 *p,*t;
     p=li;
     while(p!=NULL){
     	t=p->prox;
-    	free(p);// libera o item da lista
+    	free(p);//libera o item da lista
     	p=t;
 	}                   
+}
+
+/*retorna total de numeros maior que o numero informado*/
+int total_numeros (Lista2 *li){
+	Lista2 *aux; int tot=0;
+	for(aux=li;aux!=NULL; aux=aux->prox){
+		tot++;
+	}
+	return tot;
+}
+
+/*retorna media dos numeros*/
+float media_numeros (Lista2 *prox){
+	Lista2 *aux; float tot, soma;
+	for(aux=prox;aux!=NULL; aux=aux->prox){
+		soma = soma + aux->info;
+		tot++;
+	}
+	return soma/tot;
+}
+
+/* retorna maior numero */
+int maior_numero(Lista2 *li){
+	int maior; Lista2 *aux = li;
+	if(aux!=NULL){
+		maior = aux->info;
+	} else {
+		return 0;
+	}
+	aux=aux->prox;
+	while(aux!=NULL){
+		if(aux->info > maior){
+			maior = aux->info;
+		}
+		aux=aux->prox;
+	}
+	return maior;
+}
+
+/* retorna menor numero */
+int menor_numero(Lista2 *li){
+	int menor; Lista2 *aux = li;
+	if(aux!=NULL){
+		menor = aux->info;
+	} else {
+		return 0;
+	}
+	aux=aux->prox;
+	while(aux!=NULL){
+		if(aux->info < menor){
+			menor = aux->info;
+		}
+		aux=aux->prox;
+	}
+	return menor;
+}
+
+/* printa os elementos adjacentes de um elemento X*/
+void vizinhos(Lista2 *li, int dado) {
+	Lista2 *aux = li;
+	if(aux==NULL){
+		printf("Lista Vazia\n\n");
+	}else{
+		for(aux=li;aux!=NULL;aux=aux->prox){
+			if(aux->info == dado) {
+				if(aux->ant != NULL){
+					printf("Anterior: %d\n", aux->ant->info);
+				}else{
+					printf("Não tem anterior\n");
+				}
+				if(aux->prox != NULL){
+					printf("Próximo: %d\n", aux->prox->info);
+				}else{
+					printf("Não tem próximo\n");
+				}
+			}	
+		}
+	}
 }
--- main.cpp
+++ main.cpp
@@ -4,155 +4,283 @@
 #include "listadupla.h"
 
 void cadastrar();
+void cadastro_automatico();
 void excluir();
 void exibe();
+void exibe_reverso();
 void buscar();
-void libera();
+void total();
+void media();
+void maior_menor();
+void vizinhos();
+void esvazia();
 
 Lista2 *lst = lista_cria(); /*Lista declarada como global
 para percorrer funções sem precisar utilizar parâmetros*/
 
 void menu(){
+	system ("cls");
 	int opcao=0;
-	printf("CRUD de Números\n\n");
-	printf("1. Cadastrar Números\n");
-	printf("2. Excluir Números\n");
-	printf("3. Exibir Números\n");
-	printf("4. Procura Número\n");
-	printf("5. Zerar Lista\n");
-	printf("6. Sair do Programa\n\n");
+	printf("-------- CRUD de Números --------");
+	lista_exibe(lst);
+	printf("1.  Cadastrar Números\n");
+	printf("2.  Cadastro Automático\n");
+	printf("3.  Excluir Números\n");
+	printf("4.  Exibir Números\n");
+	printf("5.  Exibir Números Inversamente\n");
+	printf("6.  Procura Número\n");
+	printf("7.  Total de Números\n");
+	printf("8.  Média de Números\n");
+	printf("9.  Maior e Menor Números\n");
+	printf("10. Vizinhos\n");
+	printf("11. Zerar Lista\n");
+	printf("12. Sair do Programa\n\n");
 	printf("Escolha uma opção: ");
-	S
-	do{
-	 	scanf("%d", &opcao);
-	} while(opcao==0 || opcao > 6);
+	
+	do scanf("%d", &opcao);
+	while(opcao==0 || opcao > 12);
 	
 	system("cls");
 	switch(opcao){	
 		case 1:
 			cadastrar();
 		break;
-			
+		
 		case 2:
-			excluir();
+			cadastro_automatico();
 		break;
 			
 		case 3:
-			exibe();
+			excluir();
 		break;
 			
 		case 4:
-			buscar();
+			exibe();
 		break;
 			
 		case 5:
-			libera();
+			exibe_reverso();
 		break;
-				
+			
 		case 6:
+			buscar();
+		break;
+		
+		case 7:
+			total();
+		break;
+		
+		case 8:
+			media();
+		break;
+		
+		case 9:
+			maior_menor();
+		break;
+		
+		case 10:
+			vizinhos();
+		break;
+						
+		case 11:
+			esvazia();
+		break;
+		
+		case 12:
 			exit(1);
 	}
 }
 
 void cadastrar(){
-	int num;
-	char repetir;
+	int num; char repetir;	
+	system ("cls");
 	
 	printf("*** Cadastro de Numeros ***");	
+	lista_exibe(lst);	
+	printf("Insira um número: ");	
+	scanf("%d", &num);
+			
+	if(lista_busca(lst, num) == NULL)
+		lst=lista_insere(lst,num);
+	else
+		printf("\nNão é possível inserir um numero repetido!\n");
+		
 	lista_exibe(lst);
 	
-	printf("Insira um número\n\n");	
-	scanf("%d", &num);
+	printf("\nDeseja inserir outro número? [s]Sim [n]Não:  ");	
+	do scanf("%c", &repetir);
+	while((repetir != 's')  && (repetir != 'n'));	
 	
-	if(lista_busca(lst, num) == NULL){
-		lst=lista_insere(lst,num);
-	}else{
-		printf("\nNão é possível inserir um numero repetido!\n");
-	}
+	if(repetir == 's')
+		cadastrar();
+	else if (repetir == 'n')
+		menu();
+}
+
+void cadastro_automatico(){
+	int inicio, fim; char repetir;	
+	system ("cls");
 	
-	printf("\nDeseja inserir outro número? [s]Sim [n]Não ");
-	do{
-	 	scanf("%c", &repetir);
-	} while((repetir != 's')  && (repetir != 'n'));
+	printf("*** Cadastro Automático de Numeros ***");
+	printf("\nInserção de vários números na lista"); 
+	lista_exibe(lst);	
 	
-	if(repetir == 's'){
-		system("cls"); cadastrar();
-	}
-	else if (repetir == 'n'){
-		system ("cls"); menu();
+	if(lista_vazia(lst) != NULL){
+		printf("Insira um número para ser o inicio:  ");	
+		scanf("%d", &inicio);
+		
+		printf("Insira um número para ser o final(deve ser maior que o início):  ");
+		do scanf("%d", &fim);
+		while (fim <= inicio);	
+			
+		for (int i=inicio; i<=fim; i++)
+			lst = lista_insere(lst, i);
+
+		lista_exibe(lst);
+		system("pause"); menu();
+	} else {
+		printf("A lista deve estar vazia para inserir automaticamente\n\n");
+		system("pause"); menu();
 	}
 }
 
 void excluir(){
-	int num;
-	char repetir;
+	int num; char repetir;	
+	system ("cls");
 	
 	printf("*** Exclusão de Números ***");
+	lista_exibe(lst);	
+	
+	printf("Exclua um número:  ");	
+	scanf("%d", &num);	
+	
+	if(lista_busca(lst, num) == NULL)
+		printf("\nNão é possível remover um número que não está na lista!\n");
+	else
+		lst=lista_retira(lst,num);		
+		
 	lista_exibe(lst);
 	
-	printf("Exclua um número\n\n");	
-	scanf("%d", &num);
+	printf("\nDeseja excluir outro número? [s]Sim [n]Não:  ");
+	do scanf("%c", &repetir);
+	while((repetir != 's')  && (repetir != 'n'));
 	
-	if(lista_busca(lst, num) == NULL){
-		printf("\nNão é possível remover um número que não está na lista!\n");
-	}else{
-		lst=lista_retira(lst,num);
-	}
-		
-	printf("\nDeseja excluir outro número? [s]Sim [n]Não ");
-	do{
-	 	scanf("%c", &repetir);
-	} while((repetir != 's')  && (repetir != 'n'));
-	
-	if(repetir == 's'){
-		system("cls"); excluir();
-	}
-	else if (repetir == 'n'){
-		system ("cls"); menu();
-	}
+	if(repetir == 's')
+		excluir();
+	else if (repetir == 'n')
+		menu();
 }
 
 void exibe(){
-	lista_exibe(lst);
-	system("pause");
 	system ("cls");
-	menu();
+	lista_exibe(lst);
+	system("pause"); menu();
+}
+
+void exibe_reverso(){
+	system ("cls");
+	lista_exibe_reverso(lst);
+	system("pause"); menu();
 }
 
 void buscar(){
-	int num;
-	char repetir;
+	int num; char repetir;	
+	system ("cls");
 	
 	printf("*** Busca de Números ***\n\n");
-	printf("Busque um número\n\n");	
-	scanf("%d", &num);
+	lista_exibe(lst);
+	printf("Busque um número:  ");	
+	scanf("%d", &num);	
 	
-	if(lista_busca(lst, num) == NULL){
+	if(lista_busca(lst, num) == NULL)
 		printf("\nNão foi achado o número!\n");
-	}else{
-		printf("\nO numero %d está na lista!\n");
-	}
+	else
+		printf("\nO numero %d está na lista!\n");	
+		
+	printf("\nDeseja buscar outro número? [s]Sim [n]Não:  ");
+	do scanf("%c", &repetir);
+	while((repetir != 's')  && (repetir != 'n'));
+		
+	if(repetir == 's')
+		buscar();
+	else if (repetir == 'n')
+		menu();
+}
+
+void total(){
+	system ("cls");
+	printf("*** Total de Números ***\n\n");
 	
-	printf("\nDeseja buscar outro número? [s]Sim [n]Não ");
-	do{
-	 	scanf("%c", &repetir);
-	} while((repetir != 's')  && (repetir != 'n'));
+	if(lst != NULL)
+		printf("O Total de números é: %d\n\n", total_numeros(lst));
+	else
+		printf("Não é possível obter o total porque a lista está vazia\n\n");
 	
-	if(repetir == 's'){
-		system("cls"); buscar();
-	}
-	else if (repetir == 'n'){
-		system ("cls"); menu();
+	system("pause"); menu();
+}
+
+void media(){
+	system ("cls");
+	printf("*** Média de Números ***\n\n");
+	
+	if(lst != NULL)
+		printf("A Média dos números é: %.2f\n\n", media_numeros(lst));
+	else
+		printf("Não é possível obter a média porque a lista está vazia\n\n");
+	
+	system("pause"); menu();
+}
+
+void maior_menor(){
+	system ("cls");
+	printf("*** Maior e Menor Números ***\n\n");
+	
+	if(lst != NULL)
+		printf("O Maior Número é: %d\nO Menor Número é: %d\n\n", maior_numero(lst), menor_numero(lst));
+	else
+		printf("Não é possível obter o maior e menor número porque a lista está vazia\n\n");
+	
+	system("pause"); menu();
+}
+
+void vizinhos(){
+	int num; char repetir;	
+	system ("cls");
+	
+	printf("*** Vizinhos ***\n\n");
+	printf("Cada elemento de uma Lista Duplamente encadeada tem um ponteiro tanto\n");
+	printf("para o seu próximo como para o seu anterior. Busque um número da lista\n"); 
+	printf("para saber o seu próximo e o seu anterior!");
+	lista_exibe(lst);
+	
+	if(lst != NULL){
+		printf("Insira um número:  ");	
+		scanf("%d", &num);
+			
+		if(lista_busca(lst, num) == NULL)
+			printf("\nNão foi achado o número!\n");
+		else
+			vizinhos(lst, num);	
+		
+		printf("\nDeseja buscar outro número? [s]Sim [n]Não:  ");
+		do scanf("%c", &repetir);
+		while((repetir != 's')  && (repetir != 'n'));
+			
+		if(repetir == 's')
+			vizinhos();
+		else if (repetir == 'n')
+			menu();
+	} else {
+		printf("Não é possível encontrar os vizinhos porque a lista está vazia\n\n");
 	}
 }
 
-void libera(){
-	lista_libera(lst);//retira elementos da lista
+void esvazia(){
+	system ("cls");
+	lista_esvazia(lst);//retira elementos da lista
 	lst = lista_cria();//cria lista novamente
 	printf("Lista zerada!\n\n");
-	system("pause");
-	system ("cls");
-	menu();
+	system("pause"); menu();
 }
 
 int main (){
